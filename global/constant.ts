eecb3c96c363a016f6dbdb8a21905f031fdb1408
dogunfx <dogunfx@gmail.com>
2025-11-03 14:06:16 +0100

    completed setup for frontend

--- global/constant.ts
+++ global/constant.ts
@@ -1,20 +1,29 @@
-export const SITE_NAME: string = "Prolific Quant ";
-export const SITE_URL: string = "prolificquant.com";
+export const SITE_NAME: string = 'Meritable Stock Trades ';
+export const SITE_URL: string = 'meritablestocktrades.vercel.app';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
-export const SITE_ADDRESS: string = "USA";
-export const SITE_DASHBOARD: string = "https://app.prolificquant.com";
-export const SITE_EMAIL: string = "support@prolificquant.com";
-export const SITE_PHONE: string = "USA";
+export const SITE_ADDRESS: string = 'USA';
+export const SITE_DASHBOARD: string =
+  'https://meritablestocktrades-app.vercel.app';
+export const SITE_EMAIL: string = 'meritablestocktrades@lh44dmgt.com';
+export const SITE_PHONE: string = 'USA';
 export const SITE_TESTIMONIES: string =
-  "https://app.prolificquant.com/api/testimonies";
+  'https://meritablestocktrades-app.vercel.app/api/testimonies';
 
 export const LIVE_CHAT = `
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '0345d336f42ed1d836a7d0c19f47b41d468271e7';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = 'cd5df0fc8a0ee288558ecc0636cea078af5e0b68';
+    window.smartsupp || (function(d) {
+        var s, c, o = smartsupp = function() {
+            o._.push(arguments)
+        };
+        o._ = [];
+        s = d.getElementsByTagName('script')[0];
+        c = d.createElement('script');
+        c.type = 'text/javascript';
+        c.charset = 'utf-8';
+        c.async = true;
+        c.src = 'https://www.smartsuppchat.com/loader.js?';
+        s.parentNode.insertBefore(c, s);
+    })(document);
+
   `;
