18da188cb7e115d823babb0c611ca891f4455320
dogunfx <dogunfx@gmail.com>
2025-11-07 14:30:21 +0100

    updated og images

--- global/constant.ts
+++ global/constant.ts
@@ -3,7 +3,7 @@ export const SITE_URL: string = 'greenlinkmarkets.com';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const SITE_ADDRESS: string = 'USA';
 export const SITE_DASHBOARD: string = 'https://app.greenlinkmarkets.com';
-export const SITE_EMAIL: string = 'meritablestocktrades@lh44dmgt.com';
+export const SITE_EMAIL: string = 'support@greenlinkmarkets.com';
 export const SITE_PHONE: string = 'USA';
 export const SITE_TESTIMONIES: string =
   'https://greenlinkmarkets.com/api/testimonies';
